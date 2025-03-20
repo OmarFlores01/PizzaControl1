e2463397e21cb944ab04f0116606a1c15000b4d3
OmarFlores01 <omarflores.x0401@gmail.com>
2025-03-19 20:05:37 -0600

    Update db.js

--- models/config/db.js
+++ models/config/db.js
@@ -2,6 +2,12 @@ require('dotenv').config(); // Cargar las variables de entorno
 
 const mysql = require('mysql2'); // Usar mysql2 en lugar de mysql
 
+// Verificación de que las variables de entorno estén definidas
+if (!process.env.DB_HOST || !process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_DATABASE || !process.env.DB_PORT) {
+    console.error('❌ Faltan algunas variables de entorno para la conexión a la base de datos.');
+    process.exit(1);  // Salir si las variables necesarias no están definidas
+}
+
 // Crear el pool de conexiones
 const db = mysql.createPool({
     connectionLimit: 10,  // Número de conexiones en el pool
