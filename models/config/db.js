9060db6b54a3a9465bb829b52b9232b0f1936863
OmarFlores01 <omarflores.x0401@gmail.com>
2025-03-19 19:35:38 -0600

    Update db.js

--- models/config/db.js
+++ models/config/db.js
@@ -1,23 +1,21 @@
-require('dotenv').config(); // Carga las variables de entorno desde .env
+require('dotenv').config(); // Carga las variables de entorno
 
-const mysql = require('mysql'); // Carga el paquete mysql
+const mysql = require('mysql');
 
-// Crear la conexión a la base de datos usando las variables de entorno
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,       // Host de la base de datos
-    user: process.env.DB_USER,       // Usuario de la base de datos
-    password: process.env.DB_PASSWORD, // Contraseña de la base de datos
-    database: process.env.DB_DATABASE, // Nombre de la base de datos
-    port: process.env.DB_PORT        // Puerto de la base de datos
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
 });
 
-// Intentar conectar a la base de datos
 db.connect((err) => {
     if (err) {
-        console.error('❌ Error al conectar a MySQL:', err);
+        console.error('❌ Error al conectar a la base de datos:', err);
         return;
     }
-    console.log('✅ Conectado exitosamente a MySQL 🚀');
+    console.log('✅ Conectado a la base de datos MySQL 🚀');
 });
 
 module.exports = db;
