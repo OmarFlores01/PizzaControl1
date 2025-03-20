5378bd3f7d567c45059666af682038286dacd5ab
OmarFlores01 <omarflores.x0401@gmail.com>
2025-03-19 19:30:40 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const cors = require('cors'); //Middleware que permite hacer peticiones   app.use()/Ejecuta código en cada petición    
+const cors = require('cors'); // Middleware que permite hacer peticiones
 const path = require('path'); // Módulo de Node.js para manejar rutas de archivos y directorios.
+const db = require('./db'); // Importa la conexión a la base de datos
 
-//Cada archivo en routes/ contiene las definiciones de las rutas para diferentes partes del sistema
+// Cada archivo en routes/ contiene las definiciones de las rutas para diferentes partes del sistema
 const authRoutes = require('./routes/authRoutes');
 const pedidoRoutes = require('./routes/pedidoRoutes');
 const productoRoutes = require('./routes/productoRoutes');
@@ -11,6 +12,15 @@ const clienteRoutes = require('./routes/clienteRoutes');
 
 const app = express();
 const PORT = 3000;
+
+// Conectar con la base de datos al iniciar el servidor
+db.connect((err) => {
+    if (err) {
+        console.error('❌ Error al conectar a la base de datos:', err);
+        return;
+    }
+    console.log('✅ Conectado a la base de datos MySQL 🚀');
+});
 
 // Sirve archivos estáticos sean accesibles
 app.use(cors());
@@ -23,14 +33,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/login.html'));
 });
 
-// permiten a los clientes/empleados interactuar con el servidor.
+// Rutas para interactuar con el servidor
 app.use('/api/auth', authRoutes);
 app.use('/api/pedidos', pedidoRoutes);
 app.use('/api/productos', productoRoutes);
 app.use('/api/inventario', inventarioRoutes);
 app.use('/api/cliente', clienteRoutes);
 
-
+// Inicia el servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
