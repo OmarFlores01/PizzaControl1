cefc1cab63cd5030e36702d3550c941f31aea1d8
OmarFlores01 <omarflores.x0401@gmail.com>
2025-03-24 10:07:33 -0600

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -115,7 +115,7 @@ async function finalizarPedido() {
             body: JSON.stringify(pedido)
         });
 
-        if (!response.ok) throw new Error(HTTP error! status: ${response.status} - ${response.statusText});
+if (!response.ok) throw new Error(`HTTP error! status: ${response.status} - ${response.statusText}`);
 
         const data = await response.json();
 
